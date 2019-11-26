1f2090e6b67f2009661d2593c69e8aa6ec2effb7
qingsong.yang <qingsong.yang@chukou1.com>
2019-11-26 15:56:33 +0800

    数据库迁移

---
+++ EasyShop.BasicImpl/HistorySql/20191126075021_createDatabase.sql
@@ -0,0 +1,73 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+CREATE TABLE [t_Table_Root] (
+    [Id] nvarchar(32) NOT NULL,
+    [CreateTime] datetime2 NOT NULL,
+    [RootName] nvarchar(50) NOT NULL,
+    [RootCode] nvarchar(50) NOT NULL,
+    [Remark] nvarchar(200) NULL,
+    CONSTRAINT [PK_t_Table_Root] PRIMARY KEY ([Id])
+);
+
+GO
+
+CREATE TABLE [t_User_Role] (
+    [Id] nvarchar(32) NOT NULL,
+    [CreateTime] datetime2 NOT NULL,
+    [RoleName] nvarchar(50) NOT NULL,
+    [RoleCode] nvarchar(50) NOT NULL,
+    [Remark] nvarchar(200) NULL,
+    CONSTRAINT [PK_t_User_Role] PRIMARY KEY ([Id])
+);
+
+GO
+
+CREATE TABLE [t_User_RoleRoot] (
+    [Id] nvarchar(32) NOT NULL,
+    [CreateTime] datetime2 NOT NULL,
+    [UserRoleId] nvarchar(32) NOT NULL,
+    [RootId] nvarchar(32) NOT NULL,
+    CONSTRAINT [PK_t_User_RoleRoot] PRIMARY KEY ([Id])
+);
+
+GO
+
+CREATE TABLE [t_User_User] (
+    [Id] nvarchar(32) NOT NULL,
+    [CreateTime] datetime2 NOT NULL,
+    [UserName] nvarchar(50) NOT NULL,
+    [Phone] nvarchar(20) NOT NULL,
+    [Password] nvarchar(10) NOT NULL,
+    [Photo] nvarchar(200) NULL,
+    [QQNumber] nvarchar(50) NULL,
+    [WeCharNumber] nvarchar(50) NULL,
+    [Email] nvarchar(50) NULL,
+    CONSTRAINT [PK_t_User_User] PRIMARY KEY ([Id])
+);
+
+GO
+
+CREATE TABLE [t_User_UserRole] (
+    [Id] nvarchar(32) NOT NULL,
+    [CreateTime] datetime2 NOT NULL,
+    [UserId] nvarchar(32) NOT NULL,
+    [RoleId] nvarchar(32) NOT NULL,
+    CONSTRAINT [PK_t_User_UserRole] PRIMARY KEY ([Id])
+);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20191126075021_createDatabase', N'3.0.1');
+
+GO
+
---
+++ EasyShop.BasicImpl/Migrations/20191126075021_createDatabase.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EasyShop.BasicImpl.Migrations
+{
+    public partial class createDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "t_Table_Root",
+                columns: table => new
+                {
+                    Id = table.Column<string>(maxLength: 32, nullable: false),
+                    CreateTime = table.Column<DateTime>(nullable: false),
+                    RootName = table.Column<string>(maxLength: 50, nullable: false),
+                    RootCode = table.Column<string>(maxLength: 50, nullable: false),
+                    Remark = table.Column<string>(maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_t_Table_Root", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "t_User_Role",
+                columns: table => new
+                {
+                    Id = table.Column<string>(maxLength: 32, nullable: false),
+                    CreateTime = table.Column<DateTime>(nullable: false),
+                    RoleName = table.Column<string>(maxLength: 50, nullable: false),
+                    RoleCode = table.Column<string>(maxLength: 50, nullable: false),
+                    Remark = table.Column<string>(maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_t_User_Role", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "t_User_RoleRoot",
+                columns: table => new
+                {
+                    Id = table.Column<string>(maxLength: 32, nullable: false),
+                    CreateTime = table.Column<DateTime>(nullable: false),
+                    UserRoleId = table.Column<string>(maxLength: 32, nullable: false),
+                    RootId = table.Column<string>(maxLength: 32, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_t_User_RoleRoot", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "t_User_User",
+                columns: table => new
+                {
+                    Id = table.Column<string>(maxLength: 32, nullable: false),
+                    CreateTime = table.Column<DateTime>(nullable: false),
+                    UserName = table.Column<string>(maxLength: 50, nullable: false),
+                    Phone = table.Column<string>(maxLength: 20, nullable: false),
+                    Password = table.Column<string>(maxLength: 10, nullable: false),
+                    Photo = table.Column<string>(maxLength: 200, nullable: true),
+                    QQNumber = table.Column<string>(maxLength: 50, nullable: true),
+                    WeCharNumber = table.Column<string>(maxLength: 50, nullable: true),
+                    Email = table.Column<string>(maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_t_User_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "t_User_UserRole",
+                columns: table => new
+                {
+                    Id = table.Column<string>(maxLength: 32, nullable: false),
+                    CreateTime = table.Column<DateTime>(nullable: false),
+                    UserId = table.Column<string>(maxLength: 32, nullable: false),
+                    RoleId = table.Column<string>(maxLength: 32, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_t_User_UserRole", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "t_Table_Root");
+
+            migrationBuilder.DropTable(
+                name: "t_User_Role");
+
+            migrationBuilder.DropTable(
+                name: "t_User_RoleRoot");
+
+            migrationBuilder.DropTable(
+                name: "t_User_User");
+
+            migrationBuilder.DropTable(
+                name: "t_User_UserRole");
+        }
+    }
+}
